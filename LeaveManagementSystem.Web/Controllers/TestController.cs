fe66cc1ab3a06e142435d9395b9968b050bb5192
Chatpong Deepet <chatpond@outlook.com>
2025-03-18 22:19:27 +0700

    [Add] initial project

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers;
+
+public class TestController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        var data = new TestViewModel
+        {
+            Name = "Tang",
+            DateOfBirth = new DateTime(1997,12,01)
+        };
+        
+            
+        return View(data);
+    }
+}
